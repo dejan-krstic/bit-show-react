1bb36ec6c07ed62d8f5f7ad1b9138f473159611d
dejan-krstic <dejanmkrstic@gmail.com>
2018-05-08 03:55:52 +0200

    app really finished

--- src/app/shows/SingleShow.js
+++ src/app/shows/SingleShow.js
@@ -36,19 +36,19 @@ export default class SingleShow extends React.Component {
                 <div className="col m5 offset-xl1">
                     <h1> {data.title} </h1>
                     <div className="akas">
-                        {data.akas.map(e => <li>{e.getInfo()}</li>)}
+                        {data.akas.map((e,i) => <li key={i} >{e.getInfo()}</li>)}
                     </div>
                     <h4>{`Seasons ${data.seasons.length}:`}</h4>
                     <div className="seasons">
-                        {data.seasons.map(e => <li>{e.getInfo()}</li>)}
+                        {data.seasons.map((e,i) => <li key={i}>{e.getInfo()}</li>)}
                     </div>
                     <h4>Cast:</h4>
                     <div className="cast">
-                        {data.cast.map(e => <li>{e.getInfo()}</li>)}
+                        {data.cast.map((e,i) => <li key={i}>{e.getInfo()}</li>)}
                     </div>
                     <h4>Crew:</h4>
                     <div className="crew">
-                        {data.crew.map(e => <li>{e.getInfo()}</li>)}
+                        {data.crew.map((e,i) => <li key={i}>{e.getInfo()}</li>)}
                     </div>
 
                 </div>
@@ -62,19 +62,19 @@ export default class SingleShow extends React.Component {
                 <div className='row'>
                     <div className="col s6 m4">
                         <div className="ep1">
-                            {data.episodes.slice(0, Math.floor(data.episodes.length / 3)).map(e => <li>{e.getInfo()}</li>)}
+                            {data.episodes.slice(0, Math.floor(data.episodes.length / 3)).map((e,i) => <li key={i}>{e.getInfo()}</li>)}
                         </div>
                     </div>
                     <br />
                     <div className="col s6 m4">
                         <div className="ep2">
-                            {data.episodes.slice(Math.floor(data.episodes.length / 3), Math.floor(2 * data.episodes.length / 3)).map(e => <li>{e.getInfo()}</li>)}
+                            {data.episodes.slice(Math.floor(data.episodes.length / 3), Math.floor(2 * data.episodes.length / 3)).map((e,i) => <li key={i}>{e.getInfo()}</li>)}
                         </div>
                     </div>
                     <br />
                     <div className="col s6 offset-s3 m4">
                         <div className="ep3">
-                            {data.episodes.slice(Math.floor(2 * data.episodes.length / 3), data.episodes.length).map(e => <li>{e.getInfo()}</li>)}
+                            {data.episodes.slice(Math.floor(2 * data.episodes.length / 3), data.episodes.length).map((e,i) => <li key={i}>{e.getInfo()}</li>)}
                         </div>
                     </div>
                 </div>
